a719b62854bd55916a786f842a93576ee20bbf61
unknown <yangruiguo@pku.edu.cn>
2022-10-21 23:11:05 +0800

    support ListTokenEvent

--- aptos_worker.py
+++ aptos_worker.py
@@ -1,8 +1,15 @@
+from tkinter import SEL_LAST
 import requests
 from config import *
 import time
+import pymysql
+from pymysql.converters import escape_string
+import datetime
 
-last_sequence_number = 2
+db = pymysql.connect(host="imart-instance-1.ccggmi9astti.us-east-1.rds.amazonaws.com", port=3306, user="admin", password="1tdhblkfcdhx2a", db="imart")
+print("Database connected.")
+cursor = db.cursor()
+last_sequence_number = 0
 
 def getEvents(start: int):
     address, event_handle, field_name = APTOS_EVENTS["listToken"]["addr"], APTOS_EVENTS["listToken"]["event_handle"], APTOS_EVENTS["listToken"]["field_name"]
@@ -20,19 +27,49 @@ def getEvents(start: int):
     return content
 
 def analyze_Events(content):
-    global last_sequence_number
+    global last_sequence_number, cursor
+    print("start analyze")
     for event in content:
         last_sequence_number = int(event["sequence_number"])
-
+        token_data = event["data"]["token_id"]["token_data_id"]
+        collection = escape_string(token_data["collection"])
+        creator = token_data["creator"]
+        name = escape_string(token_data["name"])
+        seller = event["data"]["seller"]
+        price = int(event["data"]["price"])
+        timestamp = float(event["data"]["timestamp"])/1000000
+        createTime = datetime.datetime.fromtimestamp(timestamp)
+        try:
+            #print(f"SELECT * FROM Collection WHERE `name`='{collection}' AND `creator`='{creator}'")
+            cursor.execute(f"SELECT * FROM Collection WHERE `name`='{collection}' AND `creator`='{creator}'")
+            collectionId = cursor.fetchone()[0]
+            #print(f"SELECT * FROM AptosToken WHERE `owner`='{seller}' AND `collection`='{collection} AND `name`='{name}'")
+            cursor.execute(f"SELECT * FROM AptosToken WHERE `owner`='{seller}' AND `collection`='{collection}' AND `name`='{name}'")
+            tokenId = cursor.fetchone()[0]
+        except Exception as e:
+            print(e)
+            continue
+        sql = f"insert into `Order` (collectionId, tokenId, seller, price, status, createTime) values ('{collectionId}', '{tokenId}', '{seller}', '{price}', 'LISTING', '{createTime}')"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+        
     
 def main():
     while True:
         content = getEvents(last_sequence_number)
         if len(content) == 0:
             time.sleep(TIME_GAP)
-
-
+        analyze_Events(content)
         
+        
+        db.close()
+        exit(0)
+  
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 import pymysql
+from pymysql.converters import escape_string
 
 db = pymysql.connect(host="imart-instance-1.ccggmi9astti.us-east-1.rds.amazonaws.com", port=3306, user="admin", password="1tdhblkfcdhx2a", db="imart")
 print("Connected.")
-#sql = "SELECT * FROM `Order`"
-# sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA='imart' AND TABLE_NAME='Order'"
+# sql = "insert into AptosToken (collectionId, rawCollectionId, owner, collection, creator, name) values ('2', 'rawCollectionId2', '0x6e8817db82d333337175f9760da0cdc8b4b3ad51e2cff9bcc6113d6a12835f8', 'test', '0x6e8817db82d333337175f9760da0cdc8b4b3ad51e2cff9bcc6113d6a12835f8', 'test')"
+sql = "SELECT * FROM `Order`"
+# sql = "DELETE FROM `Order` WHERE id=2"
+# sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA='imart' AND TABLE_NAME='AptosToken'"
+# Order结构
 # (('id',), ('collectionId',), ('tokenId',), ('seller',), ('buyer',), ('price',), ('status',), ('createTime',))
 # ((1, 1, 1, '0x1ca65d884045cfedc8fc0185117e53ca0a8a0f8fd37270fd3e5934476f6756e4', None, 1.0, 'LISTING', datetime.datetime(2022, 10, 13, 6, 3, 2)),)
+# Collection结构
+# (('id',), ('rawCollectionId',), ('chain',), ('metadataType',), ('category',), ('contractName',), ('name',), ('creator',), ('description',), ('cover',), ('logo',), ('ordersCount',), ('volume',), ('floorPrice',))
+# AptosToken结构
+# (('id',), ('collectionId',), ('rawCollectionId',), ('rawTokenId',), ('owner',), ('collection',), ('creator',), ('description',), ('name',), ('uri',))
+
 print(sql)
+# try:
+#     with db.cursor() as cursor:
+#         cursor.execute(sql)
+#         db.commit()
+# except Exception as e:
+#     print(e)
+#     db.rollback()
+
 try:
     with db.cursor() as cursor:
         cursor.execute(sql)
