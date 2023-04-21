fe011ba868571ecc074c415c1f0e6b24940d691d
Amovane <amovane@163.com>
2023-04-21 16:50:02 +0800

    chore: minor fixes

--- evm/src/scripts/create_gallery.ts
+++ evm/src/scripts/create_gallery.ts
@@ -1,8 +1,8 @@
 import { BigNumber, ethers } from "ethers";
-import { ALCHEMY_API, CONTRACT_CURATION, PRIVATE_KEY_ALICE } from "../config";
+import { INFURA_API, CONTRACT_CURATION, PRIVATE_KEY_ALICE } from "../config";
 import { Curation__factory } from "../typechain";
 
-const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_API);
+const provider = new ethers.providers.JsonRpcProvider(INFURA_API);
 const signer = new ethers.Wallet(PRIVATE_KEY_ALICE, provider);
 const Curation = Curation__factory.connect(CONTRACT_CURATION, provider);
 async function main() {
