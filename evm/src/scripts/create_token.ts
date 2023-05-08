e4a6e8767207d1a028ec6fbb2267052626ed0cba
Amovane <amovane@163.com>
2023-05-08 15:34:07 +0800

    chore: tweak script

--- evm/src/scripts/create_token.ts
+++ evm/src/scripts/create_token.ts
@@ -1,25 +1,25 @@
 import { ethers } from "ethers";
 import {
-  CONTRACT_SINGLE_COLLECTIVE,
-  PRIVATE_KEY_ALICE,
-  ALCHEMY_API,
+  CONTRACT_MULTIPLE_COLLECTIVE,
+  PRIVATE_KEY_CICI,
+  INFURA_API,
 } from "../config";
-import { SingleCollective__factory } from "../typechain";
+import { MultipleCollective__factory } from "../typechain";
 
-const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_API);
-const signer = new ethers.Wallet(PRIVATE_KEY_ALICE, provider);
-const SingleCollective = SingleCollective__factory.connect(
-  CONTRACT_SINGLE_COLLECTIVE,
+const provider = new ethers.providers.JsonRpcProvider(INFURA_API);
+const signer = new ethers.Wallet(PRIVATE_KEY_CICI, provider);
+const MultipleCollective = MultipleCollective__factory.connect(
+  CONTRACT_MULTIPLE_COLLECTIVE,
   provider
 );
 async function main() {
-  const tx = await SingleCollective.connect(signer).mint(
+  const tx = await MultipleCollective.connect(signer).mint(
     "",
-    signer.address,
-    "https://mixverse-spaces.s3.amazonaws.com/mixverse-gallery-1.json",
-    { gasLimit: 3050000, gasPrice: 1500000000 }
+    10000,
+    "https://imart-nft.s3.amazonaws.com/imart/1683188049.json"
   );
-  console.log(tx);
+  const txReceipt = await tx.wait();
+  console.log(txReceipt);
 }
 
 main().catch((error) => {
