6845217be8760aebe57f0992f6beb53d8a6bb776
Amovane <amovane@163.com>
2023-07-19 14:26:10 +0800

    chore: update seed script

--- evm/prisma/seed.ts
+++ evm/prisma/seed.ts
@@ -1,35 +1,35 @@
 import { PrismaClient } from "@prisma/client";
-
+import * as env from "dotenv";
+env.config();
 async function main() {
   const client = new PrismaClient();
-  const blockNo = 8446918;
+  const { ID, BLOCK_NUMBER } = process.env;
+
+  if (!ID) throw Error("missing id");
+  if (!BLOCK_NUMBER) throw Error("missing id");
+
+  const id = +`${ID}`;
+  const blockNo = +`${BLOCK_NUMBER}`;
+  const data = {
+    buy_event_excuted_offset: blockNo,
+    list_event_excuted_offset: blockNo,
+    delist_event_excuted_offset: blockNo,
+    create_offer_excuted_offset: blockNo,
+    accept_offer_excuted_offset: blockNo,
+    cancel_offer_excuted_offset: blockNo,
+    creation_token_created_excuted_offset: blockNo,
+    creation_collection_created_excuted_offset: blockNo,
+    single_collective_created_excuted_offset: blockNo,
+    multiple_collective_created_excuted_offset: blockNo,
+    gallery_excuted_offset: blockNo,
+    exhibit_excuted_offset: blockNo,
+    lottery_excuted_offset: blockNo,
+    curation_offer_excuted_offset: blockNo,
+  };
   const result = await client.eventOffset.upsert({
-    where: { id: 1 },
-    update: {
-      buy_event_excuted_offset: -1,
-      list_event_excuted_offset: -1,
-      delist_event_excuted_offset: -1,
-      create_offer_excuted_offset: -1,
-      accept_offer_excuted_offset: -1,
-      cancel_offer_excuted_offset: -1,
-      creation_token_created_excuted_offset: blockNo,
-      creation_collection_created_excuted_offset: blockNo,
-      single_collective_created_excuted_offset: blockNo,
-      multiple_collective_created_excuted_offset: blockNo,
-    },
-    create: {
-      id: 1,
-      buy_event_excuted_offset: -1,
-      list_event_excuted_offset: -1,
-      delist_event_excuted_offset: -1,
-      create_offer_excuted_offset: -1,
-      accept_offer_excuted_offset: -1,
-      cancel_offer_excuted_offset: -1,
-      creation_token_created_excuted_offset: blockNo,
-      creation_collection_created_excuted_offset: blockNo,
-      single_collective_created_excuted_offset: blockNo,
-      multiple_collective_created_excuted_offset: blockNo,
-    },
+    where: { id },
+    update: data,
+    create: { id, ...data },
   });
   console.log(result);
 }
